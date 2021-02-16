cdd5625bbcd63e49cb97de6d2e4132496bec3eda
Kangmingu <kangmin1012>
2021-02-16 18:14:54 +0900

    타이틀 색상 그래디언트 및 텍스트 스타일 설정

--- app/src/main/java/paytalab/clone/passorder/presentation/ui/CloneApplication.kt
+++ app/src/main/java/paytalab/clone/passorder/presentation/ui/CloneApplication.kt
@@ -1,17 +1,48 @@
 package paytalab.clone.passorder.presentation.ui
 
 import android.app.Application
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.HiltAndroidApp
+import paytalab.clone.passorder.R
 import paytalab.clone.passorder.domain.util.EncryptHelper
 
 @HiltAndroidApp
 class CloneApplication : Application() {
+
+    companion object {
+        private lateinit var cloneApplication: CloneApplication
+        fun getInstance(): CloneApplication = cloneApplication
+    }
+
     override fun onCreate() {
         super.onCreate()
-
+        cloneApplication = this
         EncryptHelper.init()
 
         // 비밀번호 관련 암호화
 //        EncryptHelper.get().encrypt("string")
+    }
+
+    fun setTitleGradient(textView: TextView): Shader {
+        val paint = textView.paint
+
+        val width = paint.measureText(textView.text.toString())
+
+        return LinearGradient(
+            0f,
+            0f,
+            width,
+            textView.textSize,
+            intArrayOf(
+                ContextCompat.getColor(this, R.color.point_02),
+                Color.parseColor("#ff2d2d")
+            ),
+            null,
+            Shader.TileMode.CLAMP
+        )
     }
 }
