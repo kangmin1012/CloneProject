2563c860c15a5b989b742783a4a9505316d9fef9
Kangmingu <kangmin1012>
2021-02-16 16:52:33 +0900

    LoginActivity 이름 변경 및 퍼미션 추가

--- app/src/main/java/paytalab/clone/passorder/presentation/ui/LoginActivity.kt
+++ app/src/main/java/paytalab/clone/passorder/presentation/ui/LoginActivity.kt
@@ -1,9 +1,10 @@
-package paytalab.clone.passorder
+package paytalab.clone.passorder.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import paytalab.clone.passorder.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
